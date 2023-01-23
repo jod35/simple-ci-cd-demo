0f9c086942a062c163b35fa878f0292199e73875
jod35 <jodestrevin@gmail.com>
2023-01-23 18:47:04 +0300

    ch(app): add tests for homepage

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
+from http import HTTPStatus
 
 # Create your tests here.
+
+class HomePageTest(TestCase):
+    def test_homepage(self):
+        response = self.client.get(reverse('list_posts'))
+
+        self.assertEqual(response.status_code,HTTPStatus.OK)
